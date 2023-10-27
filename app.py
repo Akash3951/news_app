af9bd07ef13c077aad54983d7ab0272747b4265c
Akash3951 <akashgupta993161@gmail.com>
2023-10-27 18:28:44 +0530

    I have made a news app

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def show_home():
+    return render_template('index.html')
+
+@app.route("/search", methods=['POST', 'GET'])
+def home():
+    url = "https://newsapi.org/v2/everything"
+    param = {
+        'q':request.form.get('search'),
+        'sortBy':'popularity',
+        'apiKey':'319f1986331947aa99f4dcb9ad51befd'
+    }
+    response = requests.get(url, params=param)
+    data = response.json()
+    articles = data['articles']
+
+    table = f"<table border=1>"
+
+    for article in articles:
+        headline = article['description']
+        url = article['url']
+        table += f"<tr><td>{headline}</td><td><a href={url}>Link</a></td></tr>"
+    table += '</table>'
+    return table
+
+    if __name__=='__main__':
+        app.run(debug=True)
